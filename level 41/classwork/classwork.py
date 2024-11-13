eb95c7c7dd84ea07e8a4ac2c74c5fafb06a3ea8e
marr0318 <Mariamgelashvili0318@gmail.com>
2024-11-13 19:47:04 +0400

    'commit'

---
+++ level 41/classwork/classwork.py
@@ -0,0 +1,8 @@
+def word_at_index(index):
+    words = ["yogurt", "banana", "cherry", "blueberry", "tomato"]
+    print(words[index])
+
+
+index = int(input("Enter a number between 0 and 5: "))
+word_at_index(index)
+
