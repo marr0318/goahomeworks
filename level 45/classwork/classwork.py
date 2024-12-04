710d5d8d426073175781c55350830c103047fe79
marr0318 <Mariamgelashvili0318@gmail.com>
2024-12-04 22:37:07 +0400

    'commit

---
+++ level 45/classwork/classwork.py
@@ -0,0 +1,29 @@
+fruits=['apple','cherry','lemon','water','melon']
+
+for i in fruits:
+    print(i)
+
+
+num=[2,6,0,3,7]
+
+for i in num:
+    print(i)
+
+
+number=[10,4,36,2,9]
+
+for i in number:
+    print(i*i)
+
+
+
+
+numbers=[1, 2, 3, 4, 5]
+
+list=[]
+
+for number in numbers:
+    list.append(number*2)
+
+print(list)
+
---
+++ level 47/classwork/classwork.py
@@ -0,0 +1,8 @@
+stri=int(input(('enter a number: ')))
+
+
+num1=1
+while num1>=1:
+    print(stri*stri)
+
+
