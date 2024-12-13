68b74e96c026b04aab1d2dea08498774d3236c4d
marr0318 <Mariamgelashvili0318@gmail.com>
2024-12-13 22:24:37 +0400

    'commit'

---
+++ level 50/classwork/classwork.py
@@ -0,0 +1,21 @@
+print(num1=int(input('Enter a first number: ')))
+print(num2=int(input('Enter a second number: ')))
+print(operator=input('Enter an operator: '))
+
+if operator== '+':
+    print(num1+num2)
+
+elif operator== '-':
+    print(num1-num2)
+
+elif operator== '/':
+    print(num1/num2)
+
+elif operator== '*':
+    print(num1*num2)
+
+elif num1/0:
+    print("eror")
+
+else:
+    print("eror")
