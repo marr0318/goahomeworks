b569ebbfb05ffc733648d84761a876700e8108df
marr0318 <Mariamgelashvili1803@gmail.com>
2025-06-13 22:09:03 +0400

    'commit'

--- level 101/classwork/script.js
+++ level 101/classwork/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // 1. შექმენით საიტი, სადაც გექნებათ დივი, მასში კი ღილაკი, 
 
 // დივში არსებული ღილაკი ჯავასკრიპტის მეშვეობით მოაშორეთ,
@@ -14,3 +15,21 @@ let div3 = document.getElementById("divv");
 let btn3 = document.getElementById("btnn");
 let pp = document.getElementById("p1");
 div3.replaceChild(pp,btn3);
+=======
+// 1. შექმენით საიტი, სადაც გექნებათ დივი, მასში კი ღილაკი, 
+
+// დივში არსებული ღილაკი ჯავასკრიპტის მეშვეობით მოაშორეთ,
+
+// 2. შექმენით საიტი, სადაც გექნებათ პარაგრაფი, ცალკ დივი და მასში კი ღილაკი, 
+
+// ჯავასკრიპტის მეშვეობით დივში არსებული ღილაკი ჩაანაცვლეთ პარაგრაფით.
+
+let div2=document.getElementById("div")
+let btn2=document.getElementById("btn")
+div2.removeChild(btn2)
+
+let div3 = document.getElementById("divv");
+let btn3 = document.getElementById("btnn");
+let pp = document.getElementById("p1");
+div3.replaceChild(pp,btn3);
+>>>>>>> c179ce58ef04408aaf72bb5b20fd57bb674353c5
--- level 102/classwork/script.js
+++ level 102/classwork/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // 1. შექმენით საიტი სადაც გექნებათ ღილაკი, ამ ღილაკზე დაამატეთ 'click' addEventListent და 
 // ფუნქციამ კონსოლში გამოიტანოს რიცხვი 5.
 
@@ -18,4 +19,26 @@ let rmvButt = document.getElementById('rmv');
 removeBtn.addEventListener('click', function() {
   bttn.removeEventListener('click', FuncTion);
   console.log('handler removed');
+=======
+// 1. შექმენით საიტი სადაც გექნებათ ღილაკი, ამ ღილაკზე დაამატეთ 'click' addEventListent და 
+// ფუნქციამ კონსოლში გამოიტანოს რიცხვი 5.
+
+// * removeEventListener-ის მეშვეობით წაუშალეთ ზემოთ შექმნილ ფუნქციას event handler.
+
+function FuncTion() {
+    console.log(5);
+}
+
+
+let bttn = document.getElementById('btn');
+
+
+bttn.addEventListener('click', FuncTion);
+
+
+let rmvButt = document.getElementById('rmv');
+removeBtn.addEventListener('click', function() {
+  bttn.removeEventListener('click', FuncTion);
+  console.log('handler removed');
+>>>>>>> c179ce58ef04408aaf72bb5b20fd57bb674353c5
 })
