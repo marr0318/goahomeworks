8b5f13a9e1337147cf883021a4320ec953afbe6c
mary <Mariamgelashvili1803@gmail.com>
2025-06-20 22:01:19 +0400

    commit

---
+++ level 006/homework/homework.py
@@ -0,0 +1,23 @@
+name=input()
+print("hello " + name)
+
+
+surname="gelashvili" #string
+age=12 #integer
+level=20,3 #float 
+
+user_name=input()
+last_name=input()
+print("hello " + user_name + last_name)
+
+#input ნიშნავს კომპიუტერში შეყვანას მონაცემის
+#output ნიშნავს კომპიუტერის სამყაროსთან კომუნიკაცია
+
+number1=input()
+number2=input()
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(number1 / number2)
+
+
---
+++ level 008/classwork/classwork.py
@@ -0,0 +1,12 @@
+age1=20
+age2="50"
+print(age1+int(age2))
+
+age3=60
+age4="80"
+print(str(age3)+age4)
+height=30
+print(int(input())+height)
+
+
+
