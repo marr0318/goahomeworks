be9bad3b2cee3b18f33054aacae14cb29f0ddb5b
mary <Mariamgelashvili1803@gmail.com>
2025-07-03 17:40:57 +0400

    commit

---
+++ level 107/classwork/script.js
@@ -0,0 +1,21 @@
+
+
+
+Let hjj = document.getelementbyid('list')
+const bjj = ['marijan', 'ninojan', 'giojan']
+for (let i =0; i < bjj.length; i += 1){
+    let name1 = document.createElement("li")
+    let name2 = document.createTextNode(bjj[i])
+    hai.appendchild(name1)
+    hai.appendchild(name2)
+}
+
+
+let name=["nin","mar","gi"]
+        for(i=0;i<name.lenght;i++){
+            let p = document.createElement("li")
+            let ujj = document.createTextNode(name[i] + i)
+            p.appendChild(name)
+        }
+        document.body.appendChild(name)
+
